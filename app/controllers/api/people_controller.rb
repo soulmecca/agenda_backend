85dd74f566cefe7c994a40bc6af2325079ec0bb4
Danny Jung <soulmecca@gmail.com>
2016-10-12 14:29:59 -0400

    start to get data from http

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,10 +1,14 @@
 class Api::PeopleController < ApplicationController
-  
+  protect_from_forgery with: :null_session, if: ->{request.format.json?}
+
   def new
       @person = Person.new
     end
 
     def create
+      puts('######################')
+      puts('CREATE')
+      
       @person = Person.new(person_params)
         if @person.save
           render json: @person, status: 201
@@ -14,22 +18,28 @@ class Api::PeopleController < ApplicationController
     end
 
     def update
-      @person = Person.find(params[:id])
-        if @person.update(person_params)
-          render json: @person, status: 200
-        else
-          render json: {status: "Failed ot update person"}
-        end
+      puts('######################')
+      puts('UPDATE')      
+    #   @person = Person.find(params[:id])
+    #     if @person.update(person_params)
+    #       render json: @person, status: 200
+    #     else
+    #       render json: {status: "Failed ot update person"}
+    #     end
     end
 
     def show
-      @person = Person.find(params[:id])
+      puts('######################')
+      puts('SHOW')      
+      # @person = Person.find(params[:id])
     end
 
     def delete
-      @person = Person.find(params[:id])
-      @person.destroy
-      render json: {result: "info is destroyed"}, status: 200
+      puts('######################')
+      puts('DELETE')      
+      # @person = Person.find(params[:id])
+      # @person.destroy
+      # render json: {result: "info is destroyed"}, status: 200
     end
 
     private
